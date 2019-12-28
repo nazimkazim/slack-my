c247a1f09c573c512e9f41b1af102799e075e840
nazimkazim <nazik_111@mail.ru>
2019-12-28 12:46:32 +0600

    empty commit

---
+++ socketio 101/chat.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const socketio = require("socket.io");
+
+const app = express();
+
+app.use(express.static(__dirname + "/public"));
+
+const expressServer = app.listen(9000);
+
+const io = socketio(expressServer);
+
+io.on("connection", socket => {
+  socket.emit("messageFromServer", { data: "Welcome to the socketio server" });
+  socket.on("messageToServer", dataFromClient => {
+    console.log(dataFromClient);
+  });
+  socket.on("newMessageToServer", msg => {
+    //console.log(msg);
+    io.emit("messageToClient", { text: msg.text });
+  });
+});
