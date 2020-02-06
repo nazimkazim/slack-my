d24c7b875c8cd53f12726c97b9ad31b15bd1d450
nazimkazim <nazik_111@mail.ru>
2020-02-06 21:20:56 +0600

    add change nsRooms list dynamically

--- socketio 101/slack/public/scripts.js
+++ socketio 101/slack/public/scripts.js
@@ -23,12 +23,35 @@ socket.on("nsList", nsData => {
   });
 });
 
+const nsSocket = io("http://localhost:9000/wiki");
+nsSocket.on("nsRoomLoad", nsRooms => {
+  console.log(nsRooms);
+  let roomList = document.querySelector(".room-list");
+  roomList.innerHTML = "";
+  let glyph = "";
+  if (nsRooms.privateRoom) {
+    glyph = "lock";
+  } else {
+    glyph = "globe";
+  }
+  nsRooms.forEach(room => {
+    roomList.innerHTML += `<li class="room"><span class="glyphicon glyphicon-${glyph}">${room.roomTitle}</span></li>`;
+  });
+  // add click listener to each room
+  let roomNodes = document.getElementsByClassName("room");
+  Array.from(roomNodes).forEach(elem => {
+    elem.addEventListener("click", e => {
+      console.log("Someone clicked on", e.target.innerText);
+    });
+  });
+});
+
 socket.on("messageFromServer", dataFromServer => {
   console.log(dataFromServer);
   socket.emit("messageToServer", { data: "Data from the client" });
 });
 
-document.querySelector("#message-form").addEventListener("submit", event => {
+document.querySelector(".message-form").addEventListener("submit", event => {
   event.preventDefault();
   let newMessage = document.querySelector("#user-message").value;
   socket.emit("newMessageToServer", { text: newMessage });
